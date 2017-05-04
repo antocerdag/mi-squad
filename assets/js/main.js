6f69a32b8740d0f297a448a5ec7752495a29f8f4
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-04 18:08:58 -0300

    cree objeto constructor y divs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-main.js
+function Misquad(nombre,apellido,edad,hobbies){
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.edad = edad;
+	this.hobbies = hobbies;
+}
+
+var fran = new Misquad("Francisca","Ojeda",28,"Roller derby");
+var cote = new Misquad("María José","Pozo",31,"Bailar");
+var mariajose = new Misquad("María José","Rodriguez",32,"Ver series");
+var cata = new Misquad("Carolina","Tapia",27,"Trekking");
+var camila = new Misquad("Camila","Saez",24,"Origami");
+var val = new Misquad("Valentina","Saavedra",26,"Medicina autogestiva");
+var marion = new Misquad("Marion","Castillo",29,"Caminar");
+var antonia = new Misquad("Antonia","Cerda",23,"Diseñar");
+
