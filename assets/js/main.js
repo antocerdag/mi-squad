51e7f00acad48de53e8c24274e4d01b6a78427ab
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-04 18:23:41 -0300

    muestra en pantalla las personas y los datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,44 @@ var val = new Misquad("Valentina","Saavedra",26,"Medicina autogestiva");
 var marion = new Misquad("Marion","Castillo",29,"Caminar");
 var antonia = new Misquad("Antonia","Cerda",23,"Diseñar");
 
+var mostrarfran = document.getElementById("fran");
+var mostrarcote = document.getElementById("cote");
+var mostrarmariajose = document.getElementById("mariajose");
+var mostrarcata = document.getElementById("cata");
+var mostrarcamila = document.getElementById("camila");
+var mostrarval = document.getElementById("val");
+var mostrarmarion = document.getElementById("marion");
+var mostrarantonia = document.getElementById("antonia");
+
+var infofran = "Nombre : " + fran.nombre + " " + fran.apellido + "<br>" 
++ "Edad : " + fran.edad + "<br>" + "Hobbies : " + "<ul><li>"+fran.hobbies+"</li></ul>" + "<br>";
+mostrarfran.innerHTML = infofran;
+
+var infocote = "Nombre : " + cote.nombre + " " + cote.apellido + "<br>" 
++ "Edad : " + cote.edad + "<br>" + "Hobbies : " + "<ul><li>"+cote.hobbies+"</li></ul>" + "<br>";
+mostrarcote.innerHTML = infocote;
+
+var infomariajose = "Nombre : " + mariajose.nombre + " " + mariajose.apellido + "<br>" 
++ "Edad : " + mariajose.edad + "<br>" + "Hobbies : " + "<ul><li>"+mariajose.hobbies+"</li></ul>" + "<br>";
+mostrarmariajose.innerHTML = infomariajose;
+
+var infocata = "Nombre : " + cata.nombre + " " + cata.apellido + "<br>" 
++ "Edad : " + cata.edad + "<br>" + "Hobbies : " + "<ul><li>"+cata.hobbies+"</li></ul>" + "<br>";
+mostrarcata.innerHTML = infocata;
+
+var infocamila = "Nombre : " + camila.nombre + " " + camila.apellido + "<br>" 
++ "Edad : " + camila.edad + "<br>" + "Hobbies : " + "<ul><li>"+camila.hobbies+"</li></ul>" + "<br>";
+mostrarcamila.innerHTML = infocamila;
+
+var infoval = "Nombre : " + val.nombre + " " + val.apellido + "<br>" 
++ "Edad : " + val.edad + "<br>" + "Hobbies : " + "<ul><li>"+val.hobbies+"</li></ul>" + "<br>";
+mostrarval.innerHTML = infoval;
+
+var infomarion = "Nombre : " + marion.nombre + " " + marion.apellido + "<br>" 
++ "Edad : " + marion.edad + "<br>" + "Hobbies : " + "<ul><li>"+marion.hobbies+"</li></ul>" + "<br>";
+mostrarmarion.innerHTML = infomarion;
+
+var infoantonia = "Nombre : " + antonia.nombre + " " + antonia.apellido + "<br>" 
++ "Edad : " + antonia.edad + "<br>" + "Hobbies : " + "<ul><li>"+antonia.hobbies+"</li></ul>" + "<br>";
+mostrarantonia.innerHTML = infoantonia;
+
